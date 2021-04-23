450f10bfb479015cab485da6cbeb3632230adbf4
mengxiangkun <mengxiangkun@meituan.com>
2021-04-23 21:36:39 +0800

    do it

--- exercises/variables/variables6.rs
+++ exercises/variables/variables6.rs
@@ -1,9 +1,8 @@
 // variables6.rs
 // Make me compile! Execute the command `rustlings hint variables6` if you want a hint :)
 
-// I AM NOT DONE
 
-const NUMBER = 3;
+const NUMBER: i32 = 3_i32;
 fn main() {
     println!("Number {}", NUMBER);
 }
