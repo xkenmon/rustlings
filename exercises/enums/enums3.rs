8291749dc173bf18a72bc80af4bdefd656422c47
xkenmon <xkenmon@gmail.com>
2021-04-24 22:17:05 +0800

    dev

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -5,6 +5,10 @@
 
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    Move(Point),
+    Echo(String),
+    ChangeColor((i32, i32, i32)),
+    Quit,
 }
 
 struct Point {
@@ -37,6 +41,9 @@ impl State {
 
     fn process(&mut self, message: Message) {
         // TODO: create a match expression to process the different message variants
+        match message {
+            Message::ChangeColor() =>
+        }
     }
 }
 
